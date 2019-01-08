7b223b65ad76e63707359b664f718934f5a581b0
Mostafa Hamed <mostafa.hamed@esmaar.com>
2019-01-08 17:08:30 +0200

    Angular Element

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PopupComponent } from './popup.component';
+import { PopupService } from './popup.service';
+
+// Include the `PopupService` provider,
+// but exclude `PopupComponent` from compilation,
+// because it will be added dynamically.
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, BrowserAnimationsModule],
+  providers: [PopupService],
+  declarations: [AppComponent, PopupComponent],
+  bootstrap: [AppComponent],
+  entryComponents: [PopupComponent],
 })
-export class AppModule { }
+export class AppModule {
+}
